339d4c5bc09fb70ee5e9a55d84e9ce749070c3de
Alejandro Villanueva <aavillanueva6@gmail.com>
2024-11-07 15:49:42 -0800

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,16 +3,14 @@ import React from 'react';
 const Footer = () => {
   return (
     <>
-      <div>FOOTER START</div>
       <footer className="container">
         <div className="row justify-content-center">
           <div className="col-auto">
-            &copy; 2022 Alejandro Villanueva
+            &copy; 2024 Alejandro Villanueva
             <span className="visible invisible"></span>
           </div>
         </div>
       </footer>
-      <div>FOOTER END</div>
     </>
   );
 };
