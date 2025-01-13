f6aa7ac3505785d34066a3b882b5e9abd1422f6f
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-01-12 23:47:20 -0800

    update to fix bootstrap custom scss compiling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import './utils/custom.css';
+import './scss/custom.scss';
 import './utils/style.css';
 import './utils/script.js';
 
